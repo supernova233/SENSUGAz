7297e723c60ddafed518d4f4035d4bfadb04cdc2
sensugaz <sensugaz@R1214InternetUnblock.local>
2015-03-19 02:04:55 +0700

    update

--- service/application/controllers/user.php
+++ service/application/controllers/user.php
@@ -10,18 +10,7 @@ Class User extends CI_Controller
 
 	public function signup()
 	{
-		$ang = file_get_contents("php://input");
-		$req = json_decode($ang);
-
-		/* json input */
-		$username = $req->username;
-		$password = $req->password;
-		$email 	  = $req->email;
-
-		if ($this->user_model->signup($username, $password, $email))
-			return $result = '{"status" : "success", "message" : "ok"}';
-		else
-			return $result = '{"status" : "faileds", "message" : "no"}';
+		
 	}
 	
 	public function logout() { }
